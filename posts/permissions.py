460afd124ca0ec9bca269134737db807f49ade3d
Clint <cdunn6754@gmail.com>
2018-10-16 09:47:36 -0600

    Gives up on isMemberOfSub.. permission.

--- posts/permissions.py
+++ posts/permissions.py
@@ -19,32 +19,4 @@ class IsPosterOrModOrAdminOrReadOnly(permissions.BasePermission):
         
         
         return (request.user == obj.poster) or admin or mod
-    
-class IsMemberOfSubOrReadOnly(permissions.BasePermission):
-    """
-    This ensures that the user creating the post
-    is a member of the sub that contains the incipient post
-    """
-    message = "You must be a member of this subReddit to create a post here."
-    def has_permission(self, request, view):
-        
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        
-        elif request.data:
-            # prevent creation unless user is member of the sub
-            post_sub_pk = get_pk_from_link(request.data['sub'])
-            user = request.user
-            user_sub_pks = [sub.pk for sub in user.subs.all()]
-            if not (post_sub_pk in user_sub_pks):
-                return False
-                    
-        return True
-
-def get_pk_from_link(url):
-    """
-    Cut out and return the pk at the end of 
-    a hyperlink url from a hyperlinkedrelated field
-    """
-    return int(re.search('[^/]+(?=/$|$)', url).group(0))
         
--- posts/views.py
+++ posts/views.py
@@ -1,20 +1,18 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-
 from .models import Post
 from .serializers import PostSerializer
-from .permissions import IsPosterOrModOrAdminOrReadOnly, IsMemberOfSubOrReadOnly
+from .permissions import IsPosterOrModOrAdminOrReadOnly
 
 class PostListView(generics.ListCreateAPIView):
     queryset=Post.objects.all()
     serializer_class=PostSerializer
     
-    permission_classes = (IsAuthenticatedOrReadOnly,)  #IsMemberOfSubOrReadOnly
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     
     def perform_create(self, serializer):
         serializer.save(poster=self.request.user)  
-    
     
 class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset=Post.objects.all()
