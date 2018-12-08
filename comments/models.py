0cce5427cb7d87cf350e2049d49d7f3356ecf6ee
Clint <cdunn6754@gmail.com>
2018-12-08 09:43:44 -0700

    Refactors the way that comments are related to posts and parents
    Now requires a parent_fn 'full name' of the style 'tx_pk' where
    x is either 1 or 2 to indicate whether it is a child or root comment
    respectively. And pk is the primary key for the 'parent'.
    
    t1_pk indicates that the comment is the child of another comment
    so the post field will be set to null and the parent field will
    be set to the comment inicated by pk.
    
    Similarly t2_pk indicates that the comment is a root comment on
    the post with supplied pk. In that case the parent will be set to
    null and post will be set to the indicated post.
    
    This avoids needing the consumer to provide a post and a parent for
    each comment creation. It was becoming a headache to need to
    provide a post when commenting on another comment that already
    knew the post.
    
    In the case of t1_pk it might be better to still set the post. I'm not
    sure. I'm leaving it blank now because it doesn't seem necessary and
    I think it makes indicating root comments easier.

--- comments/models.py
+++ comments/models.py
@@ -6,7 +6,7 @@ from posts.models import Post
 
 class Comment(MPTTModel):
     poster = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
     
     body = models.TextField()
     upvotes = models.IntegerField(default=0)
--- comments/serializers.py
+++ comments/serializers.py
@@ -9,20 +9,25 @@ class CommentSerializer(serializers.ModelSerializer):
     
     post = serializers.SlugRelatedField(
         slug_field='title',
-        queryset = Post.objects.all()
+        read_only=True
     )
     poster = serializers.SlugRelatedField(
         slug_field='username',
         queryset = User.objects.all()
     )
     parent = serializers.PrimaryKeyRelatedField(
-        queryset=Comment.objects.all(),
+        read_only=True,
         allow_null=True
+    )
+    parent_fn = serializers.CharField(
+        write_only=True
     )
     
     class Meta:
         model = Comment
-        fields = ('poster', 'post', 'body', 'upvotes', 'parent', 'pk')
+        fields = (
+            'post', 'poster', 'parent', 'body', 'upvotes', 'parent_fn', 'pk',
+        )
         
     # def validate(self, data):
     #     """
@@ -35,11 +40,30 @@ class CommentSerializer(serializers.ModelSerializer):
     #         )
     #     return data
     
+    def validate_parent_fn(self, value):
+        """
+        the parent_id must begin with 'tx_' where x is either
+        1 or 2 to indicate that the comment was made on another
+        comment or a post, respectively. Follows the idea from
+        the reddit api.
+        """
+        # TODO get a regex
+        if not (value.startswith("t") and value[2] == "_"):
+            raise serializers.ValidationError("parent_fn must be a 'full name' "
+                "and begin with either t1_ or t2_")
+        return value
+    
     def create(self, validated_data):
-        print(validated_data)
-        parent = validated_data.get('parent')
-        if parent:
-            validated_data['post'] = parent.post
+        
+        parent_fn = validated_data.pop('parent_fn')
+        parent_pk = parent_fn[3:]
+        print("\nout: {}, [1]: {}\n".format(parent_fn, parent_fn[1]))
+        
+        if int(parent_fn[1]) == 1:
+            validated_data['parent'] = Comment.objects.get(pk=parent_pk)
+        elif int(parent_fn[1]) == 2:
+            validated_data['post'] = Post.objects.get(pk=parent_pk)
+        
         return super().create(validated_data)
     
 class CommentTreeSerializer(serializers.ModelSerializer):
