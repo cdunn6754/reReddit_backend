2caf41249906f13156174dc2fca155210ececc54
Clint <cdunn6754@gmail.com>
2018-12-22 12:58:55 -0700

    Adds commenting vote_state for home view
    There is still a bug in that the backend does not
    interpret a double vote as an unvote.

--- posts/views.py
+++ posts/views.py
@@ -17,6 +17,22 @@ class PostListView(ListAPIView):
     
     permission_classes = (IsAuthenticatedOrReadOnly,)
     
+    def get_serializer_context(self):
+        """
+        It will really speed up some vote lookups in the
+        serializer if we can turn a username into a user_pk at this point.
+        This is only relevant if the consumer provides a get param 'username'
+        """
+        context = super().get_serializer_context()
+        username = self.request.query_params.get('username')
+        if username:
+            try:
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                return context
+            context['post_user_pk'] = user.pk
+        return context
+    
 class PostDetailView(RetrieveUpdateDestroyAPIView):
     queryset=Post.objects.all()
     serializer_class=PostSerializer
@@ -40,6 +56,16 @@ class SubPostListView(ListAPIView):
         queryset = Post.objects.filter(subreddit__title=sub_title).order_by(order_by)
         return queryset
     
+    def get(self, request, *args, **kwargs):
+        # Be sure the sub exist before anything else
+        sub_title = self.kwargs.get('sub_title', None)
+        try:
+            Sub.objects.get(title=sub_title)
+        except Sub.DoesNotExist:
+            return Response({'detail': "This subreddit does not exist"},
+                            status=status.HTTP_404_NOT_FOUND)
+        return self.list(request, *args, **kwargs)
+    
     def get_serializer_context(self):
         """
         It will really speed up some vote lookups in the
@@ -54,15 +80,5 @@ class SubPostListView(ListAPIView):
             except User.DoesNotExist:
                 return context
             context['post_user_pk'] = user.pk
-        return context
-    
-    def get(self, request, *args, **kwargs):
-        # Be sure the sub exist before anything else
-        sub_title = self.kwargs.get('sub_title', None)
-        try:
-            Sub.objects.get(title=sub_title)
-        except Sub.DoesNotExist:
-            return Response({'detail': "This subreddit does not exist"},
-                            status=status.HTTP_404_NOT_FOUND)
-        return self.list(request, *args, **kwargs)
+        return context    
         
