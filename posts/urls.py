5246a3d9808085cee683dc3597cfa434bb869ec6
Clint <cdunn6754@gmail.com>
2019-01-01 12:57:05 -0700

    Fixes a type in urls for post

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path, re_path
+from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post-list'),
-    path('(<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),    
     path('subreddit-list/<slug:sub_title>/',
          views.SubPostListView.as_view(),
          name='sub-post-list'),
