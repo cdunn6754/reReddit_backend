abbbd572437b97d324c8629a76f71ca63dcfe125
Clint <cdunn6754@gmail.com>
2019-01-15 09:45:47 -0700

    Adds validation for subreddit title
    Prevents naming collisions with the psuedo-subreddits like
    'Home' and 'New'. Also updates the field type of sub.title to
    a SlugField to prevent spaces.

--- subs/models.py
+++ subs/models.py
@@ -1,12 +1,17 @@
 from django.db import models
-from django.db.models.signals import post_save
-from django.core.exceptions import  ValidationError
-from django.dispatch import receiver
+from django.core.exceptions import  FieldError
 
 class Sub(models.Model):
+    protected_subreddit_titles = [
+        'Popular',
+        'New',
+        'Hot',
+        'Home',
+    ]
+    
     created = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=150, unique=True)
-    description = models.CharField(max_length=500, blank=True)
+    title = models.SlugField(max_length=40, unique=True)
+    description = models.CharField(max_length=1000, blank=True)
     
     moderators = models.ManyToManyField(
         'redditors.User',
@@ -16,8 +21,15 @@ class Sub(models.Model):
     
     def __str__(self):
         return "subReddit: {}".format(self.title)
-
-
-
         
-    
+    def save(self, *args, **kwargs):
+        """
+        Prevent creation of subreddits that use names
+        needed for the psuedo subreddits
+        """
+        if self.title.title() in self.protected_subreddit_titles:
+            message = "The subreddit title '{}' is reserved".format(self.title)
+            raise FieldError(message)
+        else:
+            super().save(*args, **kwargs)
+        
--- subs/serializers.py
+++ subs/serializers.py
@@ -20,3 +20,11 @@ class SubSerializer(serializers.HyperlinkedModelSerializer):
         model = Sub
         fields = ('pk', 'created', 'title', 'description',
                   'moderators', 'members')
+                  
+    def validate_title(self, value):
+        """
+        Prevent title collisions with those of the psuedo subreddits
+        """
+        if value.title in Sub.protected_subreddit_titles:
+            message = "The subreddit title '{}' is reserved".format(value)
+            raise serializers.ValidationError(message)
